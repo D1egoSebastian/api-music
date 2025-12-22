e410018a519609dacedc8654fa63cced6c47be6b
D1egoSebastian <sebastiandiegorr@gmail.com>
2025-12-22 13:12:42 -0400

    metodo de validator y metodo de guardar nuevo user

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 //imports
 const validate = require("../helpers/validator")
+const User = require("../models/user");
+const bycrpt = require("bcrypt");
 
 const test = async (req, res) => {
         try{
@@ -39,14 +41,52 @@ const register = async (req, res) => {
         
 
         //control de usuarios duplicados
+        const userDuplicated = await User.find({
+            $or: [
+                {email: params.email.tolowerCase()},
+                {nick: params.nick.tolowerCase()}
+            ]
+        })
+
+        if(!userDuplicated || userDuplicated.length >=1){
+            return res.status(400).send({
+                status: "error",
+                message: "Usuario duplicado"
+            })
+        }
+
 
         //cifrar la contra
+        try{
+            const hashedPassword = await bycrpt.hash(params.password, 10);
+            params.password = hashedPassword;
+        }catch(e){
+            return res.status(500).send({
+                status: "error",
+                message: "Error al cifrar la contraseña"
+            })
+        }
+        
 
         //crear objeto del usuario
+        let userToSave = new User(params);
 
         //guardar usuario en la db
+        let UserSave = await userToSave.save();
+
+        if(!UserSave){
+            return res.status(500).send({
+                status: "error",
+                message: "Error al guardar el usuario"
+            })
+        }
 
         //limpiar objeto a devolver
+        return res.status(200).send({
+            status: "success",
+            message: "Usuario registrado correctamente",
+            user: UserSave
+        })
 
         //devolver res
         return res.status(200).send({
